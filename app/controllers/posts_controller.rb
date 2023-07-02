fd18ba2459ea7a4cda11bcf8b4636c5cbc54755b
Moesha-Githae <moesha.githae@student.moringaschool.com>
2023-07-02 19:35:29 +0300

    done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,22 @@
 class PostsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-
   def index
     posts = Post.all
     render json: posts
   end
-
   def show
     post = Post.find(params[:id])
     render json: post
   end
 
+  def short_content
+    post = Post.find(params[:id])
+    render json: post, serializer: AuthorPostsSerializer
+  end
+
   private
 
   def render_not_found_response
-    render json: { error: "Post not found" }, status: :not_found
+render json: { error: "Post not found" }, status: :not_found
   end
-
 end
